acc7114acd7f2d505802d813b466c0c9c50324ac
omerkeskin <omer.keskin.deu@hotmail.com>
2020-04-04 22:43:17 +0300

    First Class Functions: Functions Returning Functions

--- passfunction.js
+++ passfunction.js
@@ -15,3 +15,25 @@ function calculateAge(el) {
 }
 
 let resultArray = arrayCalculate(years, calculateAge);
+
+// Functions returning functions
+
+function interviewQuestion(job) {
+    if (job === 'designer'){
+        return function (name) {
+           console.log(name + ' can you explain what UIX design is?');
+        }
+    }else if(job === 'teacher'){
+        return function (name) {
+            console.log('What is your branch? Dear ' +name);
+        }
+    }else{
+        return function (name) {
+            console.log('Hello ' +name+ ' What do you do?');
+        }
+    }
+
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+interviewQuestion('designer')('Merve');
